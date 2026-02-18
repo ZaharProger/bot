c13269df4026e343df6e1f8e21510ce01ccdd6bf
Zahar Domolego <domolegozahar@gmail.com>
2026-02-18 15:35:47 +0800

    Minor fixes

--- src/services.py
+++ src/services.py
@@ -45,7 +45,7 @@ class BotService(ABC):
     
 
     def _get_random_chatbot_settings(self):
-        found_settings = ChatBotSettings.select().where(ChatBotSettings.model >> None)
+        found_settings = ChatBotSettings.select().where(ChatBotSettings.model.is_null(False))
         return None if len(found_settings) == 0 else choice(found_settings)
     
 
